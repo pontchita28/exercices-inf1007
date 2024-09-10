33eba82f6b639ccaa069199694e6f2050255b926
pontchita28 <120283336+pontchita28@users.noreply.github.com>
2024-09-10 16:32:18 -0400

    "Commencement de résolution des exercices"

--- ch03_1/exercice.py
+++ ch03_1/exercice.py
@@ -17,7 +17,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return (angle_degs * math.pi)/ 180
+    return 0.0
 
 
 def to_degrees(angle_rads: float) -> tuple:
