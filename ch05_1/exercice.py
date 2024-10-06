49d3b4e169a05327ff9980d20b92b0b477aada3c
pontchita28 <120283336+pontchita28@users.noreply.github.com>
2024-10-06 13:23:26 -0400

    Réponses aux exos et tous tests passés!

--- ch05_1/exercice.py
+++ ch05_1/exercice.py
@@ -5,29 +5,125 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    # prendre un nombre et retourner sa valeur absolue
+    number_list = list(str(number))
+    if "-" not in number_list:
+        return number
+    else:
+        # enlever le premier index qui a le "-"
+        number_list.pop(0)
+        # joindre la liste pour donner un str
+        new_num_str = "".join(number_list)
+        # mettre le nombre sous la forme d'un float 
+        abs_num = float(new_num_str)
+
+        return abs_num
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
+    # besoin de créer une nouvelle liste pour recevoir ces noms
+    name_list = []
 
-    return [""]
+    # transformer les prefixes en listes 
+    prefixes_list = list(prefixes)
+
+    for i in range(0, len(prefixes_list)):
+        name = prefixes_list[i] + suffixe
+        name_list.append(name)
+
+    return name_list
 
 
+
+import math 
 def prime_integer_summation() -> int:
-    return 0
+    # somme des 100 premiers nombres premiers excluant 1 
+    prime_list = [2, 3, 5]
+    number = 6
+
+    # besoin de savoir s'il y a un nombre qui divise par 1 et la racine carré du nombre 
+    while len(prime_list) < 100:
+        is_prime = True
+        for i in range(0, len(prime_list)):
+            if number % prime_list[i] == 0:
+                is_prime = False
+                number += 1 
+                break
+
+        if is_prime == True:
+            prime_list.append(number)
+            number += 1 
+
+
+    # somme
+    somme = 0
+    for i in range(0, len(prime_list)):
+        somme = somme + prime_list[i]
+
+    return somme 
+
+
 
 
 def factorial(number: int) -> int:
-    return 0
+    num = 1 
+
+    multiplication = 1
+    while num <= number:
+        multiplication = multiplication * num
+        num += 1
+
+    return multiplication
 
 
 def use_continue() -> None:
-    pass
+    for i in range(1, 11):
+        if i != 5:
+            print(i)
+        else:
+            continue
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    new_list = []
+    for i in range(0, len(groups)):
+        for j in range(0, len(groups[i])):
+            if 3 < len(groups[i]) <= 10:
+                if groups[i][j] >= 18:
+                    if groups[i][j] <= 70: 
+                        if groups[i][j] != 50:
+                            new_list.append(True)
+                            break
+                        else: 
+                            if 25 in groups[i]:
+                                new_list.append(True)
+                                break
+                            else:
+                                new_list.append(False)
+                                break
+                    else: 
+                        if 25 in groups[i]:
+                            new_list.append(True)
+                            break
+                        else:
+                            new_list.append(False)
+                            break
+                else:
+                    if 25 in groups[i]:
+                        new_list.append(True)
+                        break
+                    else:
+                        new_list.append((False))
+                        break
+            else:
+                new_list.append((False))
+                break
+
+                            
+    return new_list
+
+
 
 
 def main() -> None:
